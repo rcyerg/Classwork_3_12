f773d627a75e349f3ef7d66aae13918a9752d0a6
rcyerg <rcyerger02@gmail.com>
2024-03-12 20:48:34 -0400

    Added user input to Items Calculator

--- src/main/java/user_items_calculator/UserItemsCalculator.java
+++ src/main/java/user_items_calculator/UserItemsCalculator.java
@@ -1,5 +1,6 @@
 package user_items_calculator;
 
+import java.util.Scanner;
 public class UserItemsCalculator {
 
 
@@ -8,23 +9,41 @@ public class UserItemsCalculator {
 
     public static void main(String[] args) {
 
-        String product = "rice";
+        String product;
 
-        int qty = 20;
+        int qty;
 
-        float price = 5.00f;
+        float price;
 
-        float miles = 10.00f;
+        float miles;
 
         float fee;
 
-        if (miles == 0 || miles <= 4) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("What is the name of the item?");
+
+        product = scan.next();
+
+        System.out.println("How many did you order?");
+
+        qty = scan.nextInt();
+
+        System.out.println("What is the price of the item?");
+
+        price = scan.nextFloat();
+
+        System.out.println("How many miles is the delivery?");
+
+        miles = scan.nextFloat();
+
+        if (miles < 5) {
             fee = 2.00f;
-            } else if (miles == 5 || miles <= 15) {
+            } else if (miles < 16) {
             fee = 5.00f;
-            } else if (miles == 16 || miles <= 25) {
+            } else if (miles < 26) {
             fee = 10.00f;
-            } else if (miles == 26 || miles <= 50) {
+            } else if (miles < 51) {
             fee = 15.00f;
             } else {
             fee = 20.00f;}
